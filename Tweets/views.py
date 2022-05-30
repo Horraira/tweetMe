d90b9823b11f87fd6897c873e0f2f7687e86a203
AbuSohan <sohan@inflexionpointbd.com>
2022-05-31 02:43:01 +0600

    REST API views

--- Tweets/views.py
+++ Tweets/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, JsonResponse
 
 from .models import *
 # Create your views here.
@@ -8,8 +8,22 @@ def home_view(request, *args, **kwargs):
     return HttpResponse("<h1> Hello Sohan </h1>")
 
 def tweet_details_view(request, tweet_id, *args, **kwargs):
+
+    """
+    REST API VIEW
+    return JSON data
+    """
+
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
     except:
-        raise Http404
-    return HttpResponse(f"<h1> Hello {tweet_id} - {obj.content} </h1>")
+        data['message'] = "Not Found"
+        status = 404
+
+    return JsonResponse(data, status=status)
+    # return HttpResponse(f"<h1> Hello {tweet_id} - {obj.content} </h1>")
